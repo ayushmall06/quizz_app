c1b85559a9714dd2595b96bd2548d74c74317fbc
Ayush Mall <ayushmall@Ayushs-MacBook-Pro.local>
2021-07-18 21:18:23 +0530

    Home Page Completed

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    double height = MediaQuery.of(context).size.height;
+
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.dark
+      )
+    );
+    return Scaffold(
+      backgroundColor: Color(0xFF2D046E),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: height/3,
+              ),
+              Center(
+                child: Image(
+                  image: AssetImage('assets/quiz.png'),
+                  width: 300,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 40,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
+                  ),
+                  child: RaisedButton(
+                    child: Text(
+                      'PLAY',
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                    color: Color(0xFFFFBA00),
+                    onPressed: () {},
+                    textColor: Colors.white,
+
+                  ),
+                ),
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
